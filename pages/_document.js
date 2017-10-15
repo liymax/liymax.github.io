4f47f0480d69a5ab72cb54f53196250cf03329e8
himiten <1066761470@qq.com>
2017-10-15 15:59:42 +0800

    add

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ export default class extends Document {
 			<html>
 			<Head>
 				<style jsx>{`body { margin: 0 } `}</style>
-				<link  href="/static/test/test.js" as="script"/>
+				<link  rel="preload" href="/static/test/test.js" as="script"/>
 			</Head>
 			<body>
 				<Main />
