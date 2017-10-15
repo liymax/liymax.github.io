79cef993e393d54fcebf04695f02bf73d9a55221
himiten <1066761470@qq.com>
2017-10-15 20:43:41 +0800

    add

--- public/static/loadscript.js
+++ public/static/loadscript.js
@@ -1,4 +1,5 @@
 let docHead=document.head,links=docHead.getElementsByTagName("link");
+console.log(process.env.NODE_ENV);
 if(process.env.NODE_ENV==="production"){
 	links.forEach((item)=>{
 		let script=document.createElement("script");
