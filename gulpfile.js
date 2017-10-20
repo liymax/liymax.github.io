e882a0e95b5acccc4f4377bdf151f975f7148ef2
himiten <1066761470@qq.com>
2017-10-20 20:54:04 +0800

    hello

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 let gulp = require('gulp');
 let git = require('gulp-git');
 
+
+let gitPaths=['./index.html','./dist/*','./src/*'];
 gulp.task('add', function(){
-	return gulp.src(['./dist/*','./src/*'])
-		.pipe(git.add());
+	return gulp.src(gitPaths).pipe(git.add());
 });
 
 gulp.task('commit',['add'], function(){
-	return gulp.src('./**/*')
-		.pipe(git.commit(undefined, {
+	return gulp.src(gitPaths).pipe(git.commit(undefined, {
 			args: '-m "auto commit"',
 			disableMessageRequirement: true
 		}));
 });
 
+gulp.task('push', function(){
+	git.push('origin', 'master', {args: " -f"}, function (err) {
+		if (err) throw err;
+	});
+});
