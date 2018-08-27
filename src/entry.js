9d15c594b0112b0087e647e683dd2eba9ab9e22f
Liyang <blanktiger@qq.com>
2018-08-27 17:01:09 +0800

    Delete entry.js

--- src/entry.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-
-import 'src/utils/viewport';
-import { hot } from "react-hot-loader";
-import App from "./app";
-const HotApp = hot(module)(App);
-render(<HotApp />, document.getElementById("root"));
