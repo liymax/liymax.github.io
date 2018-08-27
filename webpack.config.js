81c6417002439c95b14860bff999699ffa47f76e
Liyang <blanktiger@qq.com>
2018-08-27 16:53:12 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,52 @@
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 const htmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const OpenBrowser = require("open-browser-webpack-plugin");
-const path = require("path");
-const hotEntry = [
-  "react-hot-loader/patch",
-  "webpack-dev-server/client?/",
-  "webpack/hot/only-dev-server"
-];
-const polyfill = [
-  "core-js/es6/map",
-  "core-js/es6/set",
-  "core-js/es6/array",
-  "core-js/es6/string",
-  "core-js/es6/promise"
-];
-let baseConfig = {
-  resolve: {
-    modules: [path.resolve(__dirname, "src"), "node_modules"],
-    alias: {
-      src: path.resolve(__dirname, "src")
-    }
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: "babel-loader",
-        include: path.resolve(__dirname, "src"),
-        exclude: /node_modules/
-      }
-    ]
-  },
-  entry: {
-    vendor: [...polyfill, "react", "react-dom"],
-    app: [...hotEntry, "./src/entry.js"]
-  },
-  output: {
-    filename: "[name].js?v=[hash:8]",
-    chunkFilename: "[name].js?v=[hash:8]",
-    path: path.resolve(__dirname, "public/dist"),
-    publicPath: "./"
-  }
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const base = {
+	resolve: {
+		modules: [
+			path.resolve(__dirname, 'node_modules'),
+			path.join(__dirname, './src')
+		]
+	},
+
+	module: {
+		rules: [
+			{test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/}
+		]
+	},
+	watch:true,
+	watchOptions: {
+		aggregateTimeout:400,
+		poll:1000,
+		ignored: /node_modules/
+	},
+	stats:{ colors:true },
+	devtool: 'cheap-module-eval-source-map'
 };
 
-const devConfig = Object.assign(
-  {
-    mode: "development",
-    plugins: [
-      new CleanWebpackPlugin(["dist"]),
-      new webpack.HotModuleReplacementPlugin(),
-      new htmlWebpackPlugin({
-        title: "github page",
-        template: "./template/index.html",
-        filename: "index.html",
-        inject: "body",
-        chunks: ["vendor", "app"]
-      }),
-      new OpenBrowser({ browser: "chrome", url: "http://localhost:3600", ignoreErrors: true })
-    ]
-  },
-  baseConfig
-);
+module.exports=Object.assign({
+	mode:"development",
+	entry:{
+		vendor: ['react', 'react-dom'],
+		game:"./src/index.js"
+	},
+	output:{
+		filename:"[name].js?v=[hash:8]",
+		path:path.resolve(__dirname,"dist"),
+		publicPath: "./"
+	},
 
-/*const prodConfig = Object.assign(
-  {
-    mode: "production",
-    plugins: [
-      new htmlWebpackPlugin({
-        title: "github page",
-        template: "./template/index.html",
-        filename: "index.html",
-        inject: "body",
-        chunks: ["vendor", "index"]
-      })
-    ]
-  },
-  baseConfig
-);*/
+	plugins:[
+		new webpack.HotModuleReplacementPlugin(),
+		new htmlWebpackPlugin({
+			title: "todo example",
+			template: "./template/index.html",
+			filename: "index.html",
+			inject: "body",
+			chunks: ["vendor","game"]
+		}),
+		new OpenBrowserPlugin({ url: 'http://localhost:9001' })
+	]
+},base);
 
-module.exports = devConfig;
