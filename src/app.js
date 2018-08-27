61d836963eee02d0fc4f2b257e178c3668f56ded
Liyang <blanktiger@qq.com>
2018-08-27 17:04:25 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,50 +1,12 @@
-import React from "react";
-import { injectGlobal } from 'styled-components';
-import WithContext from "./utils/WithContext";
-import store from "./reducers/index";
-import actions from "./actions/index";
-import { StoreContext } from "./context";
-import Router from "./routes/index";
-const StoreProvider = WithContext(store, actions, StoreContext);
+import { hot } from 'react-hot-loader';
+import React from 'react';
+//import {loadScript} from "../utils/common";
+import HelloWorld from "./components/helloworld";
 
-injectGlobal`
-  * {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    font: inherit;
-    vertical-align: baseline;
-  }
-/* HTML5 display-role reset for older browsers */
-  article, aside, details, figcaption, figure, 
-  footer, header, hgroup, menu, nav, section {
-    display: block;
-  }
-  body {
-    line-height: 1;
-  }
-  ol, ul {
-    list-style: none;
-  }
-  blockquote, q {
-    quotes: none;
-  }
-  blockquote:before, blockquote:after,
-  q:before, q:after {
-    content: '';
-    content: none;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
-`;
+//loadScript('/js/tfjs@0.12.5.js');
 
-export default function App() {
-  return (
-    <StoreProvider>
-      <Router />
-    </StoreProvider>
-  );
+function App(){
+	return ( <HelloWorld /> );
 }
+
+export default hot(module)(App)
