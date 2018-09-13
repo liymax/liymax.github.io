a5776fe1b4829e704967a491ef9e90b5afcae81f
Liyang <blanktiger@qq.com>
2018-09-13 17:03:11 +0800

    Create server.js

---
+++ personal-practice/mock/server.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(3030, () => {
+  console.log("JSON Server is running at port 3030");
+});
