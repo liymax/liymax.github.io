e55277790fab604099277e5f8b407dea0e79f08a
Liyang <blanktiger@qq.com>
2018-09-14 11:05:44 +0800

    Update dispatch.js

--- personal-practice/dispatch.js
+++ personal-practice/dispatch.js
@@ -1,26 +1,41 @@
-class Dispatch {
-    constructor() {
-        this.events = {}
-    }
-    on(eventname, cb) {
-        this.events[eventname] = cb;
-    }
-    emit(eventname, ...args) {
-        let fn = this.events[eventname];
-        if ("function" === typeof fn) {
-            fn.apply(null, args);
-        } else {
-            let errMsg = eventname + ' is not a subscription,or its callback is not a function';
-            throw new Error(errMsg);
+
+(function() {
+    class Dispatch {
+        constructor() {
+            this.events = Object.create(null);
+        }
+        on(eventname, cb) {
+            let cbList = this.events[eventname] || [];
+            cbList.push(cb);
+            this.events[eventname] = cbList;
+        }
+        emit(eventname, ...args) {
+            let cbList = this.events[eventname] || [];
+            if (cbList.length === 0) {
+                let errMsg = eventname + ' is not a subscription,or its callback not exist';
+                throw new Error(errMsg);
+            } else {
+                cbList.forEach(fn=>{
+                    if ("function" === typeof fn) {
+                        fn.apply(null, args);
+                    } else {
+                        let errMsg = 'this callback is not a function';
+                        throw new Error(errMsg);
+                    }
+                }
+                )
+            }
+
         }
     }
-}
-let dispatcher = new Dispatch();
-dispatcher.on("hello", (name)=>{
-    console.log("hello", name);
-}
-);
-setTimeout(()=>{
-    dispatcher.emit('hello', 'scott');
-}
-, 1000)
+    let dispatcher = new Dispatch();
+    dispatcher.on("hello", (name)=>{
+        console.log("hello", name);
+    }
+    );
+    setTimeout(()=>{
+        dispatcher.emit('hello', 'scott');
+    }
+    , 1000)
+
+}())
