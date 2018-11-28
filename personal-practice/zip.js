6e90321618c8def3a5268147d731318eddde08b7
Liyang <blanktiger@qq.com>
2018-11-28 15:04:05 +0800

    Update zip.js

--- personal-practice/zip.js
+++ personal-practice/zip.js
@@ -1,40 +1,38 @@
 const fs = require('fs');
 const path = require('path');
 const archiver = require('archiver');
+let { version } = require("../package");
 
-let rootDir = path.resolve(__dirname, '../');
-let output = fs.createWriteStream(rootDir + '/public/dist.zip');
-let archive = archiver('zip', {
-	zlib: { level: 9 }
-});
+zip(version);
+function zip(ver){
+	let rootDir = path.resolve(__dirname, '../');
+	let output = fs.createWriteStream(rootDir + `/release/ksc_dist${ver}.zip`);
+	let archive = archiver('zip', { zlib: { level: 9 } });
 
-output.on('close', function() {
-	console.log(archive.pointer() + ' total bytes');
-	console.log('archiver has been finalized and the output file descriptor has closed.');
-});
-
-output.on('end', function() {
-	console.log('Data has been drained');
-});
-
-archive.on('warning', function(err) {
-	if (err.code === 'ENOENT') {
-		console.log('No such file or directory')
-	} else {
+	output.on('close', function() {
+		console.log(archive.pointer() + ' total bytes');
+		console.log('archiver has been finalized and the output file descriptor has closed.');
+	});
+	output.on('end', function() {
+		console.log('Data has been drained');
+	});
+	archive.on('warning', function(err) {
+		if (err.code === 'ENOENT') {
+			console.log('No such file or directory')
+		} else {
+			throw err;
+		}
+	});
+	archive.on('error', function(err) {
 		throw err;
-	}
-});
+	});
 
-archive.on('error', function(err) {
-	throw err;
-});
+	archive.pipe(output);
+	archive.directory('dist/','', (data)=>{
+		return data;
+	});
 
-archive.pipe(output);
+	archive.finalize();
+}
 
-archive.directory('tests/','', (data)=>{
-	let {name} = data;
-	if(name.includes('eslintrc')) return false;
-	return data;
-});
-//archive.glob(rootDir+'tests/**/*');
-archive.finalize();
+
