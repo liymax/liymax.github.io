a588de68680798688a12c75fe5385ad5b7ec6edb
Liyang <blanktiger@qq.com>
2019-07-05 14:33:07 +0800

    Update gulpfile.js

--- personal-practice/gulpfile.js
+++ personal-practice/gulpfile.js
@@ -1,31 +1,56 @@
-const del = require('del');
-const gulp = require('gulp');
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+const { exec } = require('child_process')
+const asyncExec = util.promisify(exec)
+const gulp = require('gulp')
+const archiver = require('archiver')
 
-let { version } = require("./package.json");
-const util = require('util');
-const { exec } = require('child_process');
-const asyncExec = util.promisify(exec);
+const { version } = require('./package.json')
 
-function newVersion(ver){
-	let arr = ver.split('.');
-	arr[2] *= 1;
-	arr[2] += 1;
-	return arr.join('.');
+function newVersion () {
+  return version.split('.').map((e, idx) => {
+    return idx === 2 ? (e * 1 + 1) : e
+  }).join('.')
 }
 
-function updateVersion() {
-	const ver = newVersion(version);
-	const cmd = `yarn version --new-version ${ver}`;
-	return asyncExec(cmd);
+function updateVersion () {
+  const newVer = newVersion()
+  const cmd = `yarn version --new-version ${newVer}`
+  return asyncExec(cmd)
 }
 
-function clean() {
-	return del(['lib/**', '!lib']);
+function getEnvSet (options = {}) {
+  return Object.entries(options).reduce((rst, item) => {
+    const [key, val] = item
+    rst += `set ${key}=${val} & `
+    return rst
+  }, '')
 }
 
-function build(){
-	const cmd = `tsc`;
-	return asyncExec(cmd);
+async function build () {
+  const envSet = getEnvSet({ WALL: 'Hello' })
+  const cmd = `${envSet}yarn build`
+  // console.log('cmd:', cmd)
+  const { stdout, stderr } = await asyncExec(cmd.trim())
+  console.log(stdout, stderr)
 }
 
-exports.default = gulp.series(updateVersion,clean,build);
+function packZip (done) {
+  const newVer = newVersion()
+  const output = fs.createWriteStream(path.resolve(__dirname) + `/release/bop-${newVer}.zip`)
+  const archive = archiver('zip', { zlib: { level: 9 } })
+
+  output.on('close', function () {
+    console.log(archive.pointer() + ' total bytes, finished!')
+    done()
+  })
+  archive.on('error', function (err) {
+    throw err
+  })
+  archive.pipe(output)
+  archive.directory('dist/', '', (data) => data)
+  archive.finalize()
+}
+
+exports.default = gulp.series(updateVersion, build, packZip)
