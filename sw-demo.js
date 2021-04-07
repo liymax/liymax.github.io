e46ee826ee937ba5ef7c6103fc5ce8a22d570b37
Liyang <blanktiger@qq.com>
2021-04-07 11:28:25 +0800

    Update sw-demo.js

--- sw-demo.js
+++ sw-demo.js
@@ -2,17 +2,23 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(Promise.resolve().then(() => {
     const { url } = event.request
     if (url.includes('sw-hello.js')) {
-      console.log(url)
-      return new Response('console.log("hello service worker")', {
+      /*return new Response('console.log("hello service worker")', {
         status: 200,
         headers: { 'Content-Type': 'application/javascript' }
+      })*/
+      const localReq = new Request('http://localhost:3005/hello.js')
+      return fetch(localReq).then(function (response) {
+        return response
+      }).catch(function (err) {
+        console.log(err)
+        return err
       })
     } else {
       return fetch(event.request).then(function (response) {
         return response
       }).catch(function (err) {
         console.log(err)
-        return new Response(err)
+        return err
       })
     }
   }))
