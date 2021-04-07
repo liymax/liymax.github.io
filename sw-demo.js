3ce1aba8dab621a7ed255b652acdf7914a2d92c5
Liyang <blanktiger@qq.com>
2021-04-07 11:01:07 +0800

    Update sw-demo.js

--- sw-demo.js
+++ sw-demo.js
@@ -1,17 +1,19 @@
 self.addEventListener('fetch', function(event) {
-  const { url } = event.request
-  if (url.includes('sw-hello.js')) {
-    console.log(url)
-    event.respondWith(new Response('console.log("hello service worker")', {
-      status: 200,
-      headers: { 'Content-Type': 'application/javascript' }
-    }))
-  } else {
-    return fetch(event.request).then(function (response) {
-      return response
-    }).catch(function (err) {
-      console.log(err)
-      event.respondWith(err)
-    })
-  }
+  event.respondWith(Promise.resolve().then(() => {
+    const { url } = event.request
+    if (url.includes('sw-hello.js')) {
+      console.log(url)
+      return new Response('console.log("hello service worker")', {
+        status: 200,
+        headers: { 'Content-Type': 'application/javascript' }
+      })
+    } else {
+      return fetch(event.request).then(function (response) {
+        return response
+      }).catch(function (err) {
+        console.log(err)
+        return new Response(err)
+      })
+    }
+  }))
 })
